b40a0daa2527e3be4232ef38dec1df4acc0de6a0
Ian Kim <kormeian@gmail.com>
2022-11-23 18:24:49 +0900

    [level 2] Title: 배달, Time: 0.92 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {
+    private static ArrayList<Edge>[] edgeList; 
+    private static int[] distance; 
+
+    private static void dijkstra() {
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        queue.add(new Edge(1, 0));
+        while (!queue.isEmpty()) {
+            Edge edge = queue.poll();
+            int vertex = edge.vertex;
+            int weight = edge.weight;
+            if (distance[vertex] < weight) {
+                continue;
+            }
+            for (int i = 0; i < edgeList[vertex].size(); i++) {
+                int vertex2 = edgeList[vertex].get(i).vertex;
+                int weight2 = edgeList[vertex].get(i).weight + weight;
+                if (distance[vertex2] > weight2) { 
+                    distance[vertex2] = weight2;
+                    queue.add(new Edge(vertex2, weight2));
+                }
+            }
+        }
+    }
+
+
+    public int solution(int N, int[][] road, int K) { 
+        int answer = 0;
+        edgeList = new ArrayList[N + 1];
+        distance = new int[N + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        for (int i = 1; i <= N; i++) {
+            edgeList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < road.length; i++) {
+            edgeList[road[i][0]].add(new Edge(road[i][1], road[i][2]));
+            edgeList[road[i][1]].add(new Edge(road[i][0], road[i][2]));
+        }
+        distance[1] = 0;
+        dijkstra();
+        for (int cost : distance) {
+            if (cost <= K) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private static class Edge implements Comparable<Edge> {
+        int vertex; 
+        int weight; 
+
+        public Edge(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+}
