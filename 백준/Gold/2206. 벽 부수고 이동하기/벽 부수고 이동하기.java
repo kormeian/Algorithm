d9323a15218fa66727852aa90fab9639de4c9462
Ian Kim <kormeian@gmail.com>
2022-11-23 10:33:52 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1192 ms, Memory: 215832 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,92 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    static class Place {
+        int y;
+        int x;
+        int dis; 
+        int drill; 
+
+        public Place(int y, int x, int dis, int drill) {
+            this.y = y;
+            this.x = x;
+            this.dis = dis;
+            this.drill = drill;
+        }
+    }
+
+    static int N, M, ans;
+    static int[][] map, visit;
+
+    static int[] dy = {-1, 1, 0, 0}; 
+    static int[] dx = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+
+        N = Integer.parseInt(str[0]); 
+        M = Integer.parseInt(str[1]); 
+
+        map = new int[N][M]; 
+        visit = new int[N][M]; 
+
+        for (int i = 0; i < N; i++) {
+            str = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(str[j]);
+                visit[i][j] = Integer.MAX_VALUE; 
+            }
+        }
+
+        ans = Integer.MAX_VALUE;
+
+        bfs(0, 0); 
+
+        if (ans == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(ans);
+    }
+
+    public static void bfs(int y, int x) {
+
+        Queue<Place> q = new LinkedList<>();
+        q.add(new Place(y, x, 1, 0)); 
+        visit[y][x] = 0; 
+
+        while (!q.isEmpty()) {
+
+            Place p = q.poll();
+
+            if (p.y == N - 1 && p.x == M - 1) {
+                ans = p.dis;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+
+                if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+                if (visit[ny][nx] <= p.drill) continue;
+                
+                if (map[ny][nx] == 0) { 
+                    visit[ny][nx] = p.drill;
+                    q.add(new Place(ny, nx, p.dis + 1, p.drill));
+                } else { 
+                    if (p.drill == 0) {
+                        visit[ny][nx] = p.drill + 1;
+                        q.add(new Place(ny, nx, p.dis + 1, p.drill + 1));
+                    }
+                }
+            }
+        }
+    }
+}
