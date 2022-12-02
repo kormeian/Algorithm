e9aef4a829122db49439b54ea4b93eee0072cdc1
Ian Kim <kormeian@gmail.com>
2022-12-02 22:20:16 +0900

    [Silver II] Title: 결혼식, Time: 188 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,54 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static class Node {
-
-		int to;
-		int weight;
-
-		public Node(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
-		ArrayList<Node>[] list = new ArrayList[n + 1];
-		for (int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
-		}
+		boolean[][] myFriends = new boolean[n + 1][n + 1];
 		for (int i = 0; i < m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			list[from].add(new Node(to, 1));
-			list[to].add(new Node(from, 1));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			myFriends[a][b] = true;
+			myFriends[b][a] = true;
 		}
-		ArrayDeque<Node> q = new ArrayDeque<>();
-		q.add(new Node(1, 0));
-		boolean[] visited = new boolean[n + 1];
-		visited[1] = true;
-		int answer = 0;
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			if (node.weight >= 2) {
-				continue;
-			}
-			for (Node next : list[node.to]) {
-				if (!visited[next.to]) {
-					visited[next.to] = true;
-					answer++;
-					q.add(new Node(next.to, node.weight + 1));
+		boolean[] guest = new boolean[n + 1];
+		for (int i = 2; i < n + 1; i++) {
+			if (myFriends[1][i]) {
+				guest[i] = true;
+				for (int j = 2; j < n + 1; j++) {
+					if (i != j && myFriends[i][j]) {
+						guest[j] = true;
+					}
 				}
+			}
+		}
+		int answer = 0;
+		for (int i = 0; i < n + 1; i++) {
+			if (guest[i]) {
+				answer++;
 			}
 		}
 		System.out.println(answer);
