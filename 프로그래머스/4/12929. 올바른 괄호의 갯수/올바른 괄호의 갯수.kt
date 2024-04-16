72324f2d7a5144b1445c873ff2459c593ea60a98
Ian Kim <kormeian@gmail.com>
2024-04-16 23:53:55 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.02 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Int): Int {
+        val dp = IntArray(n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for (i in 2..n) {
+            for (j in 1..i) {
+                dp[i] += dp[j - 1] * dp[i - j]
+            }
+        }
+        return dp[n]
+    }
+}
